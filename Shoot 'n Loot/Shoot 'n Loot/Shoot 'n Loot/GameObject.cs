0a46cfd96fb948321f8f991ae2e169ed9660fcec
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-01-07 15:03:44 +0100

    Started on a gameobject class for things to extend

---
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shoot__n_Loot
+{
+    class GameObject
+    {
+        public Vector2 Position { get; protected set; }
+        public Vector2 Size { get; protected set; }
+        public Vector2 Center { get { return Position + Size / 2; } }
+        public Rectangle Hitbox { get { return new Rectangle((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y); } }
+        public void Update();
+        public void Draw(SpriteBatch spriteBatch);
+    }
+}
---
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shoot__n_Loot
+{
+    class Player : GameObject
+    {
+    }
+}
