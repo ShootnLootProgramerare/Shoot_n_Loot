34459c5d87b2b6549003b0212a48709794de7717
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-01-07 17:29:33 +0100

    Removed some stuff

--- Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Game1.cs
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Game1.cs
@@ -47,7 +47,6 @@ namespace Shoot__n_Loot
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
@@ -66,11 +65,6 @@ namespace Shoot__n_Loot
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
