07ae71e5c8f43cf5825405dea496a3cd82387e03
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-01-21 12:47:07 +0100

    renamed johans random

--- Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Game1.cs
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Game1.cs
@@ -17,7 +17,7 @@ namespace Shoot__n_Loot
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         public static Point ScreenSize { get { return new Point(1200, 750); } }
-        public static Random R;
+        public static Random random;
         internal static Player player;
         internal static Enemy enemy;
 
@@ -55,7 +55,7 @@ namespace Shoot__n_Loot
             objectsToRemove = new List<GameObject>();
             Camera.Scale = 1;
             Camera.Origin = new Vector2(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height) / (2 * Camera.Scale);
-            R = new Random();
+            random = new Random();
 
             base.Initialize();
         }
