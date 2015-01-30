2c85bca21a5fcc9fb854689feab8297f5cbf9095
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-01-30 17:04:32 +0100

    added a bunch of stuff to WeaponPart

--- Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/WeaponPart.cs
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/WeaponPart.cs
@@ -9,18 +9,27 @@ namespace Shoot__n_Loot
     {
         public enum PartType { Base, Barrel, Mag }
 
-        public  PartType Type { get; private set; }
+        public PartType Type { get; private set; }
 
         public float DamageMod { get; private set; }
-        public float SpeedMod { get; private set; }
+        public float BulletSpeedMod { get; private set; }
+        public float ShootSpeedMod { get; private set; }
+        public float ReloadSpeedMod { get; private set; }
         public byte MagSizeMod { get; private set; }
+        public bool MakesAuto { get; private set; }
+        public Weapon.AmmoType[] AcceptableAmmo { get; private set; }
+        //maybe armor penetration?
 
-        public WeaponPart(PartType type, float damageMod, float speedMod, byte magSizeMod)
+        public WeaponPart(PartType type, float damageMod, float speedMod, byte magSizeMod, bool makesAuto, float reloadSpeedMod, Weapon.AmmoType[] acceptableAmmo)
         {
             this.Type = type;
             this.DamageMod = damageMod;
-            this.SpeedMod = speedMod;
+            this.BulletSpeedMod = speedMod;
             this.MagSizeMod = magSizeMod;
+            this.MakesAuto = makesAuto;
+            this.ReloadSpeedMod = reloadSpeedMod;
+            this.AcceptableAmmo = acceptableAmmo;
+            this.BulletSpeedMod = BulletSpeedMod;
         }
     }
 }
