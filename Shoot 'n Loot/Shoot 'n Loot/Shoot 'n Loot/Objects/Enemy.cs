85be6ff4531687a0cfe2a7108afc88fcefb9068c
Johannes Larsson <mrja1lisepic@gmail.com>
2015-02-06 13:01:45 +0100

    new grass tile and adjusted enemy map collider

--- Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Objects/Enemy.cs
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Objects/Enemy.cs
@@ -20,6 +20,7 @@ namespace Shoot__n_Loot
         public override string Type { get { return TYPE; } }
 
         public override Rectangle MapCollider { get { return new Rectangle(base.MapCollider.X + (int)(base.MapCollider.Width * .4f), base.MapCollider.Y + (int)(base.MapCollider.Height * .75f), (int)(base.MapCollider.Width / 5), (int)(base.MapCollider.Height * .25f)); } }
+        public override Rectangle BulletCollider { get { return new Rectangle(base.BulletCollider.X + (int)(base.MapCollider.Width / 3f), base.MapCollider.Y, base.MapCollider.Width / 3, base.MapCollider.Height); } }
 
         public enum EnemyType { Fisherman = 1, enemy2 = 2, enemy3 = 3 };
 
