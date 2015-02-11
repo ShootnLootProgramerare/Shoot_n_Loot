dd7b9e5fd51f78b6dc30e0fc6e4edfbfc1756695
jg0ne <johan_huang123@hotmail.com>
2015-02-11 21:02:35 +0100

    Started making HP Bar

---
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/Objects/HPBar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Shoot__n_Loot.Scenes;
+
+namespace Shoot__n_Loot
+{
+    class HPBar : GameObject
+    {
+        public Vector2 Position { get; set; }
+        public int Health { get; set; }
+
+        public HPBar(Vector2 position, int health)
+        {
+            this.Position = position;
+            this.Health = health;
+        }
+
+        //percentage = currentHP / maxHp
+        public void Draw(SpriteBatch spriteBatch, float percentage)
+        {
+
+        }
+    }
+}
