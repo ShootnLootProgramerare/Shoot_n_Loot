d54a013e9dfc3ae30c5fe8b69bf050ca73f84175
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-03-08 23:25:25 +0100

    adjusted weight on items

--- Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/WeaponClasses/Items.cs
+++ Shoot 'n Loot/Shoot 'n Loot/Shoot 'n Loot/WeaponClasses/Items.cs
@@ -26,13 +26,13 @@ namespace Shoot__n_Loot.WeaponClasses
             ligthAmmo,
             mediumAmmo,
             heavyAmmo,
-            new ItemProperties(1, 1, 1, TextureManager.enemy1, 1, "Pink blob\nDoes nothing"),
-            new ItemProperties(2, 1, 1, TextureManager.medicineItem, 10, "Heal 1 point", healht1),
-            new ItemProperties(1, 1, 1, TextureManager.house, 1, "Auto: true\nAlso newLine", new WeaponPart(WeaponPart.PartType.Base, 1, 1, 30, true, 1, 1, new Weapon.AmmoType[] { Weapon.AmmoType.Medium } )),
-            new ItemProperties(1, 1, 1, TextureManager.enemy2, 1, "Weaponpart 2", new WeaponPart(WeaponPart.PartType.Barrel, 1, 1, 0, false, 10, 10, new Weapon.AmmoType[] { Weapon.AmmoType.Light } )),
-            new ItemProperties(1, 1, 1, TextureManager.house, 1, "Other house like part", new WeaponPart(WeaponPart.PartType.Base, 1, 3, 10, false, 2, 10, new Weapon.AmmoType[] { Weapon.AmmoType.Heavy } )),
-            new ItemProperties(1, 1, 1, TextureManager.beans, 5, "Can of Beans\nRestore 10 health points", Hunger),
-            new ItemProperties(1, 1, 1, TextureManager.enemy1, 10, "Bandage\nReduce bleeding", Bandage)
+            new ItemProperties(1, 1, 3, TextureManager.enemy1, 1, "Pink blob\nDoes nothing"),
+            new ItemProperties(2, 1, .01f, TextureManager.medicineItem, 10, "Heal 1 point", healht1),
+            new ItemProperties(1, 1, .3f, TextureManager.house, 1, "Auto: true\nAlso newLine", new WeaponPart(WeaponPart.PartType.Base, 1, 1, 30, true, 1, 1, new Weapon.AmmoType[] { Weapon.AmmoType.Medium } )),
+            new ItemProperties(1, 1, .4f, TextureManager.enemy2, 1, "Weaponpart 2", new WeaponPart(WeaponPart.PartType.Barrel, 1, 1, 0, false, 10, 10, new Weapon.AmmoType[] { Weapon.AmmoType.Light } )),
+            new ItemProperties(1, 1, .35f, TextureManager.house, 1, "Other house like part", new WeaponPart(WeaponPart.PartType.Base, 1, 3, 10, false, 2, 10, new Weapon.AmmoType[] { Weapon.AmmoType.Heavy } )),
+            new ItemProperties(1, 1, .5f, TextureManager.beans, 5, "Can of Beans\nRestore 10 hunger points", Hunger),
+            new ItemProperties(1, 1, .2f, TextureManager.enemy1, 10, "Bandage\nReduce bleeding", Bandage)
         } ;
 
         public static Item GetAmmo(Weapon.AmmoType type, Vector2 position)
